5ec4c8781ce330d2e7fbb5434fad46965cf769aa
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2024-12-07 16:06:46 +0700

    :mute: silent changes: add description for all constructors #4

--- init.go
+++ init.go
@@ -1,10 +1,23 @@
 package wrapify
 
+// NewPagination creates a new instance of the `pagination` struct.
+//
+// This function initializes a `pagination` struct with its default values.
+//
+// Returns:
+//   - A pointer to a newly created `pagination` instance.
 func NewPagination() *pagination {
 	p := &pagination{}
 	return p
 }
 
+// NewMeta creates a new instance of the `meta` struct.
+//
+// This function initializes a `meta` struct with its default values,
+// including an empty `CustomFields` map.
+//
+// Returns:
+//   - A pointer to a newly created `meta` instance with initialized fields.
 func NewMeta() *meta {
 	m := &meta{
 		CustomFields: map[string]interface{}{},
@@ -12,11 +25,24 @@ func NewMeta() *meta {
 	return m
 }
 
+// NewHeader creates a new instance of the `header` struct.
+//
+// This function initializes a `header` struct with its default values.
+//
+// Returns:
+//   - A pointer to a newly created `header` instance.
 func NewHeader() *header {
 	h := &header{}
 	return h
 }
 
+// NewWrap creates a new instance of the `wrapper` struct.
+//
+// This function initializes a `wrapper` struct with its default values,
+// including an empty map for the `Debug` field.
+//
+// Returns:
+//   - A pointer to a newly created `wrapper` instance with initialized fields.
 func NewWrap() *wrapper {
 	w := &wrapper{
 		Debug: &map[string]interface{}{},
