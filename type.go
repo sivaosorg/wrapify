8c3d7188376551cba328cce9c6d2b0a61f189d03
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2025-12-28 23:07:23 +0700

    :recycle: refactor: update codebase #2

--- type.go
+++ type.go
@@ -284,41 +284,6 @@ type Pair[T any, U any] struct {
 	Second U
 }
 
-// OptionsConfig defines the configuration options for pretty-printing JSON data.
-// It allows customization of width, prefix, indentation, and sorting of keys.
-// These options control how the JSON output will be formatted.
-//
-// Fields:
-//   - Width: The maximum column width for single-line arrays. This prevents arrays from becoming too wide.
-//     Default is 80 characters.
-//   - Prefix: A string that will be prepended to each line of the output. Useful for adding custom prefixes
-//     or structuring the output with additional information. Default is an empty string.
-//   - Indent: The string used for indentation in nested JSON structures. Default is two spaces ("  ").
-//   - SortKeys: A flag indicating whether the keys in JSON objects should be sorted alphabetically. Default is false.
-type OptionsConfig struct {
-	// Width is an max column width for single line arrays
-	// Default is 80
-	Width int `json:"width"`
-	// Prefix is a prefix for all lines
-	// Default is an empty string
-	Prefix string `json:"prefix"`
-	// Indent is the nested indentation
-	// Default is two spaces
-	Indent string `json:"indent"`
-	// SortKeys will sort the keys alphabetically
-	// Default is false
-	SortKeys bool `json:"sort_keys"`
-}
-
-// Style is the color style
-type Style struct {
-	Key, String, Number [2]string
-	True, False, Null   [2]string
-	Escape              [2]string
-	Brackets            [2]string
-	Append              func(dst []byte, c byte) []byte
-}
-
 // ///////////////////////////
 // Section unexported types
 // ///////////////////////////
