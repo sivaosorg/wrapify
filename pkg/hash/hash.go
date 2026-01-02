5cb8f6fe12e02bc1235d427bb7e07d019a941731
arisnguyenit97 <nguyenhuuphuocit97@gmail.com>
2026-01-03 00:22:49 +0700

    :recycle: refactor: update codebase #2

--- pkg/hash/hash.go
+++ pkg/hash/hash.go
@@ -1,6 +1,9 @@
 package hash
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+)
 
 // HashValue generates a 64-bit hash value for a single value with options.
 // This is the primary hashing function.
@@ -38,4 +41,50 @@ func HashValue(value any, options *Options) (uint64, error) {
 
 	hasher.hash.Reset()
 	return hasher.hashValue(reflect.ValueOf(value), nil)
+}
+
+// Hash generates a 64-bit hash value for the given data.
+// It accepts variadic arguments - if only one argument is provided, it hashes
+// that value. If multiple arguments are provided, it hashes them as a tuple.
+// The last argument can optionally be *Options.
+//
+// Examples:
+//
+//	hash, err := Hash(myStruct)                    // Single value
+//	hash, err := Hash(val1, val2, val3)            // Multiple values
+//	hash, err := Hash(myStruct, opts)              // With options
+//	hash, err := Hash(val1, val2, val3, opts)      // Multiple values with options
+//
+// The hash is deterministic: identical values always produce identical hashes.
+//
+// Returns:
+//   - uint64: The computed hash value (never zero for valid inputs)
+//   - error: Non-nil if hashing fails
+func Hash(data ...any) (uint64, error) {
+	if len(data) == 0 {
+		return 0, fmt.Errorf("pkg.hash: no data provided")
+	}
+
+	var opts *Options
+	var values []any
+
+	if len(data) > 0 {
+		if o, ok := data[len(data)-1].(*Options); ok {
+			opts = o
+			values = data[:len(data)-1]
+		} else {
+			values = data
+		}
+	}
+
+	if len(values) == 0 {
+		return 0, fmt.Errorf("pkg.hash: no data provided")
+	}
+
+	// If single value, hash it directly
+	if len(values) == 1 {
+		return HashValue(values[0], opts)
+	}
+
+	return HashValue(values, opts)
 }
